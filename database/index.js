62432d231747421811ae013ee17cde0c8f7f4df6
Ethem-Deli <e.deli5595@gmail.com>
2025-11-17 09:36:20 +0300

    DATABASE POOL FIXED

--- database/index.js
+++ database/index.js
@@ -6,12 +6,9 @@ require("dotenv").config();
  * Works both locally and in production
  * *************** */
 
-const { Pool } = require("pg");
-require("dotenv").config();
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
